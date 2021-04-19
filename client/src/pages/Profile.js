852ba7eaf6adf77230e5cbaf5aa1561bf99e826e
Luke Arenas <lukedarenas@gmail.com>
2021-04-18 18:45:49 -0600

    styled profile with no posts

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -7,8 +7,10 @@ import {
   setSelectedPost,
   setEditing
 } from '../store/actions/PostAction'
+import cameraIcon from '../assets/camera-png.png'
 import { getMonth } from '../helperFunction'
 import { useHistory } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const mapStateToProps = ({ authState, postState }) => {
   return { authState, postState }
@@ -46,6 +48,8 @@ const Profile = (props) => {
     <div className="detail-page">
       <div className="profile-header-container">
         <h3 className="white">@{props.authState.currentUser.username}</h3>
+      </div>
+      <div className="flex-container">
         <div className="relative">
           <img
             src={profilePicture}
@@ -57,46 +61,45 @@ const Profile = (props) => {
               <ProfilePicture />
             </div>
           ) : (
-            <div>
-              <button onClick={() => props.setEditing()} className="edit-btn">
-                Edit
-              </button>
+            <div onClick={() => props.setEditing()} className="edit-btn">
+              <img src={cameraIcon} alt="edit button" className="edit-camera" />
             </div>
           )}
         </div>
       </div>
-      {filteredPosts.length
-        ? filteredPosts.map((post, idx) => (
-            <div
-              key={idx}
-              onClick={() => handleClick(post)}
-              className="post-card"
-            >
-              <div className="grid-container">
-                <div className="poster-container">
-                  <img
-                    src={post.User.profilePicture}
-                    alt={post.User.username}
-                    className="profile-picture"
-                  />
-                  <h4>@{post.User.username}</h4>
-                </div>
+
+      {filteredPosts.length ? (
+        filteredPosts.map((post, idx) => (
+          <div
+            key={idx}
+            onClick={() => handleClick(post)}
+            className="post-card"
+          >
+            <div className="grid-container">
+              <div className="poster-container">
+                <img
+                  src={post.User.profilePicture}
+                  alt={post.User.username}
+                  className="profile-picture"
+                />
+                <h4>@{post.User.username}</h4>
               </div>
-              <img
-                src={post.image}
-                alt={post.caption}
-                className="preview-pic"
-              />
-              <h4 className="align-text-left">{post.caption}</h4>
-              <h4 className="align-text-left date">{`${getMonth(
-                post
-              )}  ${post.createdAt.slice(8, 10)}, ${post.createdAt.slice(
-                0,
-                4
-              )}`}</h4>
             </div>
-          ))
-        : null}
+            <img src={post.image} alt={post.caption} className="preview-pic" />
+            <h4 className="align-text-left">{post.caption}</h4>
+            <h4 className="align-text-left date">{`${getMonth(
+              post
+            )}  ${post.createdAt.slice(8, 10)}, ${post.createdAt.slice(
+              0,
+              4
+            )}`}</h4>
+          </div>
+        ))
+      ) : (
+        <div>
+          No moments yet! Pin your first one <NavLink to="/post">here</NavLink>.
+        </div>
+      )}
     </div>
   )
 }
